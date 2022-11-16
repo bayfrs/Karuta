5fe2a95037a66bd45f568f0806f68b5b17e963bd
bayfrs <113220899+bayfrs@users.noreply.github.com>
2022-11-16 15:52:56 +0700

    update Karuta.py

--- Karuta.py
+++ Karuta.py
@@ -1,9 +1,7 @@
 
-
 import gdown
 
-
-# same as the above, but with the folder ID
-id = "1NdSujVIFdgx8IdxW3fOWs64IYY9yaI7Q"
-gdown.download_folder(id=id, quiet=True, use_cookies=False)
+url = "https://drive.google.com/file/d/1NdSujVIFdgx8IdxW3fOWs64IYY9yaI7Q/view?usp=drivesdk"
+output = "karuta.zip"
+gdown.download(url, output, quiet=False)
 
